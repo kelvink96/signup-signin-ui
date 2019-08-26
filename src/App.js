083b7f44f8aafcbea8e3b65d05a1ecda338a7704
kelvink96 <kelvin.kiptum@alternateltd.com>
2019-08-26 19:40:44 +0300

    added not styled sign up form input fields

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.svg'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="App-aside">
+                <img src={logo} className="App-logo" alt="logo"/>
+            </div>
+            <div className="App-form">
+                <div className="Page-switcher">
+                    <a href="" className="Page-switcher-link active">sign up</a>
+                    <a href="" className="Page-switcher-link">sign in</a>
+                </div>
+                <div className="Form-title">
+                    <a href="" className="Form-title-link">sign up</a> or
+                    <a href="" className="Form-title-link"> sign in</a>
+                </div>
+                <div className="Form-content">
+                    <form action="" className="Form-fields">
+                        <div className="Form-field">
+                            <label htmlFor="fullName">full name</label>
+                            <input type="text" className="Form-full-name" id="fullName"
+                                   placeholder="enter your full name"/>
+                        </div>
+                        <div className="Form-field">
+                            <label htmlFor="password">password</label>
+                            <input type="password" className="Form-full-name" id="password" placeholder="password"/>
+                        </div>
+                        <div className="Form-field">
+                            <label htmlFor="email">e-mail</label>
+                            <input type="email" className="Form-full-name" id="email" placeholder="enter your email"/>
+                        </div>
+                        <div className="Form-field">
+                            <label htmlFor="termsAndConditions">
+                                <input type="checkbox" id="termsAndConditions"/> i agree with all statements in <a
+                                href="" className="termsLink">terms and conditions of service</a>
+                            </label>
+                        </div>
+                        <div className="Form-field">
+                            <button className="Signup-btn">sign up</button>
+                            <a href="" className="alreadyAMemberLink">already a member?</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
