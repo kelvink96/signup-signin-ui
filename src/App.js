d02b65df120e5c53f92d264db72d995f00a878f5
kelvink96 <kelvin.kiptum@alternateltd.com>
2019-08-31 10:54:04 +0300

    created SignUpForm as a component

--- src/App.js
+++ src/App.js
@@ -1,51 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from "react-router-dom";
 import logo from './logo.svg'
 import './App.css';
+import SignUpForm from "./pages/SignUpForm";
 
 function App() {
     return (
-        <div className="App">
-            <div className="App-aside">
-                <img src={logo} className="App-logo" alt="logo"/>
-            </div>
-            <div className="App-form">
-                <div className="Page-switcher">
-                    <a href="#" className="Page-switcher-link active">Sign Up</a>
-                    <a href="#" className="Page-switcher-link">Sign In</a>
+        <Router>
+            <div className="App">
+                <div className="App-aside">
+                    <img src={logo} className="App-logo" alt="logo"/>
                 </div>
-                <div className="Form-title">
-                    <a href="" className="Form-title-link">sign in</a> <small>or</small>&nbsp;
-                    <a href="" className="Form-title-link active">sign up</a>
-                </div>
-                <div className="Form-content">
-                    <form action="" className="Form-fields">
-                        <div className="Form-field">
-                            <label htmlFor="fullName">full name</label><br/>
-                            <input type="text" className="Form-full-name" id="fullName"
-                                   placeholder="enter your full name"/>
-                        </div>
-                        <div className="Form-field">
-                            <label htmlFor="password">password</label><br/>
-                            <input type="password" className="Form-full-name" id="password" placeholder="password"/>
-                        </div>
-                        <div className="Form-field">
-                            <label htmlFor="email">e-mail</label><br/>
-                            <input type="email" className="Form-full-name" id="email" placeholder="enter your email"/>
-                        </div>
-                        <div className="Form-field">
-                            <label htmlFor="termsAndConditions"><br/>
-                                <input type="checkbox" id="termsAndConditions"/> i agree with all statements in <a
-                                href="" className="Terms-link">terms and conditions of service</a>
-                            </label>
-                        </div>
-                        <div className="Form-field Signup-btn-container">
-                            <br/><button className="Signup-btn">sign up</button>
-                            <a href="" className="alreadyAMemberLink">already a member?</a>
-                        </div>
-                    </form>
+                <div className="App-form">
+                    <div className="Page-switcher">
+                        <a href="#" className="Page-switcher-link active">Sign Up</a>
+                        <a href="#" className="Page-switcher-link">Sign In</a>
+                    </div>
+                    <div className="Form-title">
+                        <Link to="/sign-in" className="Form-title-link">sign in</Link> <small>or</small>&nbsp;
+                        <a href="" className="Form-title-link active">sign up</a>
+                    </div>
+                    <Route exact path="/" component={SignUpForm}>
+                    </Route>
+                    <Route path="/sign-in">
+                        <h1>Sign In</h1>
+                    </Route>
                 </div>
             </div>
-        </div>
+        </Router>
     );
 }
 
